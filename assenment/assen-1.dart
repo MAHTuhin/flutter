7517fe9c828792e527a004db9b59b6d5985f36a8
MAHTuhin <mah95tuhin@gmail.com>
2024-03-13 23:37:19 +0600

    oop class

---
+++ assenment/assen-1.dart
@@ -0,0 +1,23 @@
+class Media{
+  void play(){
+    print('Playing Cricket');
+  }
+}
+class Song extends Media{
+  String department;
+ Song(this.department);
+  @override
+  void play()
+  {
+    super.play();
+    print('Playing Footbal');
+  }
+}
+void main(){
+  Media obj= Media();
+  Song obj1= Song('cse');
+  obj.play();
+  obj1.play();
+  print(obj1.department);
+
+}
